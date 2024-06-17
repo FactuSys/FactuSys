c73eb2b7831d96eadd886a406933bfee06c8c9bf
ATOMOSX <juand.lopezm@uqvirtual.edu.co>
2024-06-16 21:46:23 -0500

    first commit

---
+++ src/main/java/org/example/factusys/FactuSysApplication.java
@@ -0,0 +1,13 @@
+package org.example.factusys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactuSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactuSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/factusys/FactuSysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.factusys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactuSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
